d844b5fcb6859479bd13cf8d3fd2360ad93c9e43
minseongkim97 <kyrok67@naver.com>
2022-07-20 19:29:20 +0900

    collection view layout & 데이터 연결

--- sesac_Book/BookCollectionViewCell.swift
+++ sesac_Book/BookCollectionViewCell.swift
@@ -8,14 +8,26 @@
 import UIKit
 
 class BookCollectionViewCell: UICollectionViewCell {
+    var backgroundColors: [UIColor] = [.systemPurple, .systemBlue, .systemPink, .systemCyan]
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var rateLabel: UILabel!
     @IBOutlet weak var movieImageView: UIImageView!
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.layer.cornerRadius = 10
+        contentView.backgroundColor = backgroundColors.randomElement()
+    }
     
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        contentView.layer.cornerRadius = 10
+        contentView.backgroundColor = backgroundColors.randomElement()
+    }
     
     func configureCell(movie: Movie) {
         titleLabel.text = movie.title
         rateLabel.text = "\(round(movie.rate*10)/10)"
+        movieImageView.image = UIImage(named: movie.title) ?? UIImage(named: "광해")!
     }
 }
